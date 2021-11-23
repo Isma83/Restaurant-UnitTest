6e9ff83351c3f34d23285d1a10f9073c9f77bc79
imunchen <ismael.munchen@capgemini.com>
2021-11-23 20:01:43 -0300

    TestCase feature

---
+++ Restaurant.Unit.Tests/CalculatorUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Restaurant.Unit.Tests
+{
+    class CalculatorUnitTests
+    {
+        private Calculator _calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _calculator = new Calculator();
+        }
+
+        [TestCase(0, 1000, 1000)]
+        [TestCase(-1, -1, -2)]
+        [TestCase(0, 0, 0)]
+        [TestCase(10000, 10000, 20000)]
+        public void Sum_ShouldReturnExpectedValue(int value1, int value2, int expectedResult)
+        {
+            // Arrange
+
+            // Act
+            var result = _calculator.Sum(value1, value2);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
