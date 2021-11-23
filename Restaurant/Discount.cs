0af4b061e3ff6314f72350a831e6769488c7393e
imunchen <ismael.munchen@capgemini.com>
2021-11-23 18:09:17 -0300

    Add CalculateProductDiscountAsync

--- Restaurant/Discount.cs
+++ Restaurant/Discount.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Restaurant
 {
@@ -8,13 +9,17 @@ namespace Restaurant
         private const decimal DiscountWinter = 0.15m;
 
 
-        public decimal CalculateProductDiscount(Product product)
+        public decimal CalculateProductDiscount(Product product) => CalculateProductDiscountInternal(product);
+
+        public Task<decimal> CalculateProductDiscountAsync(Product product) => Task.Run(() => CalculateProductDiscountInternal(product));
+
+        private decimal CalculateProductDiscountInternal(Product product)
         {
             if (DateTime.Now.Month == 2 || DateTime.Now.Month == 3)
             {
                 return product.Total - (product.Total * DiscountSummer);
             }
-            
+
             if (DateTime.Now.Month == 9 || DateTime.Now.Month == 10)
             {
                 return product.Total - (product.Total * DiscountWinter);
